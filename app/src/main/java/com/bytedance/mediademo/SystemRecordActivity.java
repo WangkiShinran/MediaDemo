777df2259930c2ec8d16b7762bb369a799023a42
Pegga <SJTU525217@sjtu.edu.cn>
2022-12-13 19:52:41 +0800

    first commit

---
+++ app/src/main/java/com/bytedance/mediademo/SystemRecordActivity.java
@@ -0,0 +1,158 @@
+package com.bytedance.mediademo;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class SystemRecordActivity extends AppCompatActivity {
+
+    private int REQUEST_VIDEO = 1001;
+    private int REQUEST_VIDEO_PATH = 1002;
+    private int PERMISSION_REQUEST_VIDEO_CODE = 1003;
+    private int PERMISSION_REQUEST_VIDEO_PATH_CODE = 1004;
+
+    private VideoView mVideoView;
+    private String takeVideoPath;
+
+    public static void startUI(Context context) {
+        Intent intent = new Intent(context, SystemRecordActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_record);
+        mVideoView = findViewById(R.id.videoview);
+    }
+
+    public void takeVideo(View view) {
+        requestVideoPermission();
+    }
+
+    public void takeVideoUsePath(View view) {
+        requestVideoAndSDCardPermission();
+    }
+
+    private void requestVideoAndSDCardPermission() {
+        boolean hasCameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+        boolean hasAudioPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
+        if (hasCameraPermission && hasAudioPermission) {
+            takeVideoUsePathHasPermission();
+        } else {
+            List<String> permission = new ArrayList<String>();
+            if (!hasCameraPermission) {
+                permission.add(Manifest.permission.CAMERA);
+            }
+            if (!hasAudioPermission) {
+                permission.add(Manifest.permission.RECORD_AUDIO);
+            }
+            ActivityCompat.requestPermissions(this, permission.toArray(new String[permission.size()]), PERMISSION_REQUEST_VIDEO_PATH_CODE);
+        }
+    }
+
+    private void takeVideoUsePathHasPermission() {
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        takeVideoPath = getOutputMediaPath();
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, PathUtils.getUriForFile(this,takeVideoPath));  // 指定保存路径
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_VIDEO_PATH);
+        }
+    }
+
+    private String getOutputMediaPath() {
+        File mediaStorageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile = new File(mediaStorageDir, "VID_" + timeStamp + ".mp4");
+        if (!mediaFile.exists()) {
+            mediaFile.getParentFile().mkdirs();
+        }
+        return mediaFile.getAbsolutePath();
+    }
+
+    private void requestVideoPermission() {
+        boolean hasCameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+        boolean hasAudioPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
+        if (hasCameraPermission && hasAudioPermission) {
+            takeVideoHasPermission();
+        } else {
+            List<String> permission = new ArrayList<String>();
+            if (!hasCameraPermission) {
+                permission.add(Manifest.permission.CAMERA);
+            }
+            if (!hasAudioPermission) {
+                permission.add(Manifest.permission.RECORD_AUDIO);
+            }
+            ActivityCompat.requestPermissions(this, permission.toArray(new String[permission.size()]), PERMISSION_REQUEST_VIDEO_CODE);
+        }
+    }
+
+    private void takeVideoHasPermission() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_VIDEO_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                takeVideoHasPermission();
+            } else {
+                Toast.makeText(this, "权限获取失败", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == PERMISSION_REQUEST_VIDEO_PATH_CODE) {
+            boolean hasPermission = true;
+            for (int grantResult : grantResults) {
+                if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                    hasPermission = false;
+                    break;
+                }
+            }
+            if (hasPermission) {
+                takeVideoUsePathHasPermission();
+            } else {
+                Toast.makeText(this, "权限获取失败", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_VIDEO && resultCode == RESULT_OK) {
+            Uri videoURI = data.getData();
+            mVideoView.setVideoURI(videoURI);
+            mVideoView.start();
+        } else if (requestCode == REQUEST_VIDEO_PATH && resultCode == RESULT_OK) {
+            Uri videoURI = data.getData();
+            mVideoView.setVideoURI(videoURI);
+            mVideoView.start();
+        }
+    }
+}
